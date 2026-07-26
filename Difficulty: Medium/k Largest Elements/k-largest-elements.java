8342d8145eb046295246f80c6943d3122de0dd90
madhu sudan <madhu7002sudan@gmail.com>
2026-07-26 08:42:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k Largest Elements/k-largest-elements.java
+++ Difficulty: Medium/k Largest Elements/k-largest-elements.java
@@ -1,14 +1,22 @@
 class Solution {
     public ArrayList<Integer> kLargest(int[] arr, int k) {
-       Arrays.sort(arr);
-        ArrayList<Integer> al=new ArrayList<Integer>();
-        int q=1;
-        for(int i=arr.length-1;i>=0;i--){
-            if(q<=k){
-                al.add(arr[i]);
-                q++;
-            }
+      
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for( int i: arr){
+            pq.add(i);
+             if(pq.size()>k){
+             pq.poll();
+             
+           
+           
+                }
         }
-        return al;
+        
+      
+         ArrayList<Integer> al=new ArrayList<Integer>(pq);
+         Collections.sort(al);
+         Collections.reverse(al);       
+        return al ;
     }
 }
