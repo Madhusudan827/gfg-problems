f38de7491a2d57fbb8b313b85dbe6d2f88ea7d48
madhu sudan <madhu7002sudan@gmail.com>
2026-09-23 19:21:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Prime Number/next-prime-number.java
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public static int nextPrime(int n) {
-           int m=n+1;
-           while(true){
-               boolean check=true;
-               for(int i=2;i<=m/2;i++){
-                    if(m%i==0){
-                        check=false;
-                        break;
-                    }
-               }
-               if(check){
-                   return m;
-               }
-               m++;
-           }
+
+      while(true){
+          n=n+1;
+          boolean primeOrnot=true;
+          for(int i=2;i<=Math.sqrt(n);i++){
+              if(n%i==0){
+                  primeOrnot=false;
+                  break;
+              }
+          }
+          if(primeOrnot){
+              return n;
+          }
+      }
       
     }
 }
